fb587e7c94d8939fc8b18918e00b82ae64f0bd16
Lucas Araraki <lucas.araraki.gomes@avanade.com>
2020-05-24 02:09:30 -0300

    feat: delete transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, FindManyOptions } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -11,7 +11,10 @@ const transactionsRouter = Router();
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
   const balance = await transactionsRepository.getBalance();
-  const transactions = await transactionsRepository.find();
+
+  const transactions = await transactionsRepository.find({
+    relations: ['category_id'],
+  });
 
   return response.json({ transactions, balance });
 });
@@ -24,11 +27,16 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+  await deleteTransactionService.execute(id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+  return response.status(500).send();
 });
 
 export default transactionsRouter;
